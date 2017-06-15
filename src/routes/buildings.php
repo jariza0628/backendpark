598621482d2b5ba770f0f294a34e07870978d026
Jeffer Ariza <Jeffer Ariza>
2017-06-15 15:00:01 -0500

    liberar espacio, desbloquear, espacio, espacios siponibles

--- src/routes/buildings.php
+++ src/routes/buildings.php
@@ -15,7 +15,7 @@ $app->add(function ($req, $res, $next) {
 // buildings*****************************************************************
 //************************************************************************************
 //************************************************************************************
-
+date_default_timezone_set('America/Bogota');
 $app->get('/api/buildings', function(Request $request, Response $response){
     $sql = "SELECT * FROM tb_edificio WHERE estado=1";
     try{
@@ -88,7 +88,11 @@ $app->get('/api/block/{id}', function(Request $request, Response $response){
 
 $app->get('/api/blockByBuilding/{id}', function(Request $request, Response $response){
     $id = $request->getAttribute('id');
-    $sql = "SELECT * FROM tb_bloque WHERE id_edificio = $id AND estado=1";
+    $sql = "SELECT `id_bloque`, `numero`, `estado`, `id_edificio`, 
+            (SELECT COUNT(*) AS espacioslibresporbloque 
+            FROM bd_park.spacelibres WHERE idbloque = id_bloque
+            AND dia=".date('d')." AND mes = ".date('m')." AND anio = ".date('Y').") AS numfreeblock 
+            FROM tb_bloque WHERE id_edificio =  $id AND estado=1";
     try{
         // Get DB Object
         $db = new db();
@@ -101,6 +105,26 @@ $app->get('/api/blockByBuilding/{id}', function(Request $request, Response $resp
     } catch(PDOException $e){
         echo '{"error": {"text": '.$e->getMessage().'}';
     }
+});
+//no se esta utlizando
+$app->get('/api/freeSpacesByBlock/{id}', function(Request $request, Response $response){
+    $id = $request->getAttribute('id');
+
+    $sql="SELECT COUNT(*) AS espacioslibresporbloque FROM bd_park.spacelibres 
+            WHERE  dia = '29' AND mes = '05' AND anio = '2017' AND  idbloque = $id";
+     try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $result = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($result);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+
 });
 
 // floors*****************************************************************
@@ -124,7 +148,12 @@ $app->get('/api/floors', function(Request $request, Response $response){
 
 $app->get('/api/floorByIdBlock/{id}', function(Request $request, Response $response){
     $id = $request->getAttribute('id');
-    $sql = "SELECT * FROM tb_piso WHERE id_bloque = $id AND estado=1";
+    $sql = "SELECT `id_piso`, `numero`, `estado`, `id_bloque`,
+    (SELECT COUNT(*) AS espacioslibresporbloque 
+    FROM bd_park.spacelibres WHERE idpiso = id_piso 
+    AND dia=".date('d')." AND mes = ".date('m')." AND anio = ".date('Y').") AS numfreefloor
+    FROM tb_piso WHERE id_bloque = $id AND estado=1";
+
     try{
         // Get DB Object
         $db = new db();
@@ -177,9 +206,10 @@ $app->get('/api/space/{id}', function(Request $request, Response $response){
 });
 
 $app->get('/api/spaceByfloorId/{id}', function(Request $request, Response $response){
+    $dia = date("d");$mes=date("m");$anio=date("Y");
     $id = $request->getAttribute('id');
     $sql = "SELECT * FROM bd_park.spacelibres
-            WHERE  dia = '29' AND mes = '05' AND anio = '2017' AND idpiso = '$id'
+            WHERE  dia = '".$dia."' AND mes = '".$mes."' AND anio = '".$anio."' AND idpiso = '$id'
             ";
     try{
         // Get DB Object
@@ -197,8 +227,9 @@ $app->get('/api/spaceByfloorId/{id}', function(Request $request, Response $respo
 //
 
 $app->get('/api/freeSpaces', function(Request $request, Response $response){
+    $dia = date("d");$mes=date("m");$anio=date("Y");
     $sql = "SELECT * FROM bd_park.spacelibres
-            WHERE  dia = '29' AND mes = '05' AND anio = '2017'
+            WHERE  dia = '".$dia."' AND mes = '".$mes."' AND anio = '".$anio."'
             ORDER BY hora
             LIMIT 0 , 5;";
      try{
@@ -216,17 +247,16 @@ $app->get('/api/freeSpaces', function(Request $request, Response $response){
 
 });
 
-//ESPACIOS LIBRES POR EDIFICIO ID
+//numero de ESPACIOS LIBRES POR EDIFICIO ID
 
 
 $app->get('/api/freeSpacesByBuilding/{id}', function(Request $request, Response $response){
+    $dia = date("d");$mes=date("m");$anio=date("Y");
+
     $id = $request->getAttribute('id');
-    $sql = "SELECT COUNT(*) AS ESPACIOSLIBRESHOY FROM tb_espacio 
-            INNER JOIN tb_piso ON tb_espacio.id_piso = tb_piso.id_piso 
-            INNER JOIN tb_bloque ON tb_bloque.id_bloque = tb_piso.id_bloque 
-            INNER JOIN tb_edificio ON tb_edificio.id_edificio = tb_bloque.id_edificio 
-            WHERE tb_edificio.id_edificio='$id' 
-            AND tb_espacio.id_espacio NOT in (SELECT id_espacio FROM tb_temp_usuario)";
+    $sql="SELECT COUNT(*) AS ESPACIOSLIBRESHOY FROM bd_park.spacelibres
+            WHERE  dia = '".$dia."' AND mes = '".$mes."' AND anio = '".$anio."'";
+
      try{
         // Get DB Object
         $db = new db();
@@ -242,7 +272,7 @@ $app->get('/api/freeSpacesByBuilding/{id}', function(Request $request, Response 
 
 });
 
-//total de esacio por edificio
+//numero total de esacio por edificio
 $app->get('/api/SpacesByBuilding/{id}', function(Request $request, Response $response){
     $id = $request->getAttribute('id');
     $sql = "SELECT COUNT(*) AS totalEspacios FROM tb_espacio 
@@ -266,14 +296,41 @@ $app->get('/api/SpacesByBuilding/{id}', function(Request $request, Response $res
 
 });
 
+//TRAER ESPACIO LIBRE CON SU CALENDARIO DISPONIBLE DE "HOY"
+$app->get('/api/SpacesWhithCalendarFree/{id}', function(Request $request, Response $response){
+    $id = $request->getAttribute('id');
+    $sql = "SELECT tb_calendario.id_calendario, tb_calendario.horario, tb_espacio.id_espacio, tb_espacio.numero,
+            tb_piso.numero AS piso, tb_bloque.numero as bloque 
+            FROM tb_calendario 
+            INNER JOIN tb_espacio ON tb_calendario.id_espacio  = tb_espacio.id_espacio  
+            INNER JOIN tb_piso ON tb_espacio.id_piso = tb_piso.id_piso 
+            INNER JOIN tb_bloque ON tb_bloque.id_bloque = tb_piso.id_bloque 
+            WHERE tb_espacio.id_espacio='$id'
+           ";
+     try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $result = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($result);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
 
-// buildings*****************************************************************
+});
+
+
+
+// operaciones con spaces*****************************************************************
 //************************************************************************************
 //************************************************************************************
 
 $app->post('/api/save/space', function(Request $request, Response $response){
-    $id_espacio = 1;
-    $id_usuario = 2;
+    $id_espacio = $request->getParam('id');;
+    $id_usuario = $request->getParam('iduser');;
 
     $sql="INSERT INTO `tb_temp_usuario` (`id_temp`, `fecha`, `estado`, `id_usuario`, `id_espacio`)
           VALUES (NULL, '".date("d/m/Y")."', '1', :id_usuario, :id_espacio);";
@@ -289,16 +346,16 @@ $app->post('/api/save/space', function(Request $request, Response $response){
         $stmt->bindParam(':id_usuario',  $id_usuario);
         
         $stmt->execute();
-        echo '{"notice": {"text": "Customer Added"}';
+        echo '{"notice": {"text": "Customer Added" '.$sql.'- esp: '.$id_espacio.'}';
     } catch(PDOException $e){
         echo '{"error": {"text": '.$e->getMessage().'}';
     }
 });
 
 // Delete desbloquear spacio
-$app->DELETE('/api/delSpaceTmp/delete/{id}', function(Request $request, Response $response){
+$app->delete('/api/delSpaceTmp/delete/{id}', function(Request $request, Response $response){
     $id = $request->getAttribute('id');
-    $sql = "DELETE FROM `tb_temp_usuario` WHERE `tb_temp_usuario`.`id_temp` = '$id'";
+    $sql = "DELETE FROM `tb_temp_usuario` WHERE `tb_temp_usuario`.`id_usuario` = '$id'";
     try{
         // Get DB Object
         $db = new db();
@@ -312,3 +369,220 @@ $app->DELETE('/api/delSpaceTmp/delete/{id}', function(Request $request, Response
         echo '{"error": {"text": '.$e->getMessage().'}';
     }
 });
+
+
+
+// usuarios***************************************************************************
+//************************************************************************************
+//************************************************************************************
+
+$app->get('/api/SpacesOccupiedByUser/{id}', function(Request $request, Response $response){
+    $id = $request->getAttribute('id');
+    $sql = "SELECT COUNT(*) AS ocupado FROM `tb_temp_usuario` WHERE `id_usuario`='$id'
+           ";
+     try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $result = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($result);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+
+});
+
+$app->get('/api/freeSpace/{info}', function(Request $request, Response $response){
+    $info = $request->getAttribute('info');
+    $idespacio = "";
+    //echo $info."<br>";
+
+    $anio1 = substr($info, 0,4);
+    $mes = substr($info, -17,2);
+    $dia = substr($info, -15,2);
+    //echo $anio1."<br>";
+    //echo $dia."<br>";
+    //echo $mes."<br>"; 
+    $anio = substr($info, -12,4);
+    $mes2 = substr($info, -8,2);
+    $dia2 = substr($info, -6,2);
+    //echo $anio."<br>";
+    //echo $dia2."<br>";
+    //echo $mes2."<br>"; 
+    $userid = substr($info, -3,1);
+    //echo $userid."<br>";
+    $jornada = substr($info, -1,1);
+    //echo $jornada."<br>";
+    //consultar el espacio por el id del usuario
+    $sql="SELECT * FROM `tb_espacio` WHERE `id_usuario`= ".$userid."";
+     try{
+        // Get DB Object
+       $db = new db();
+        // Connect
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        foreach($stmt as $row)
+         $idespacio = $row[0];
+        $result = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+    } catch(PDOException $e){
+        //echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+
+    //difeecia entre los meses
+
+    $diferenciames = (int) $mes2 - (int) $mes;
+    $diferenciadia = (int) $dia2 - (int) $dia;
+    $diferenciaanio = (int) $anio - (int) $anio1;
+    //echo "diferencia de mese ".$diferenciames."<br>";
+    //echo "diferencia de anio1 ".$diferenciaanio."<br>";
+    //echo "diferencia de dias ".$diferenciadia."<br>";
+    //si mes de 31 dias
+    $quemes = (int) $mes;
+    $diasdelmes = cuantodistieneelmes($quemes); //establecer cuantos dias tine el mes selecionado 31, 30 o febrero
+    
+    //echo "el mes tiene:  ".$diasdelmes."<br>";
+
+    //mes = mes and anio1 = anio1
+    if($diferenciames == 0 AND $diferenciaanio == 0){
+       
+        $d1 = (int) $dia;
+        $d2 = (int) $dia2;
+        while ( $d1 <=  $d2) {
+            $sql = "INSERT INTO `tb_calendario` (`id_calendario`, `dia`, `mes`, `anio`, `horario`, `id_espacio`) VALUES 
+            (NULL, '".$d1."', '".$mes."', '".$anio."', '".$jornada."', '".$idespacio."');";
+            //echo $sql."<br>";
+
+            try{
+                // Get DB Object
+                $db = new db();
+                // Connect
+                $db = $db->connect();
+                $stmt = $db->prepare($sql);
+                $stmt->execute();
+                //echo '{"notice": {"text": "Customer Added" '.$sql.'}';
+            } catch(PDOException $e){
+                //echo '{"error": {"text": '.$e->getMessage().'}';
+            }
+                    $d1 = $d1 + 1;
+        }
+    }elseif($diferenciames!=0 AND $diferenciaanio==0){//mes =! mes and anio1 0 anio1
+        $m1 = (int) $mes;$m2 = (int) $mes2;
+        $d1 = (int) $dia;$d2 = (int) $dia2;
+        $diai = $d1;
+        $diaf = $diasdelmes;
+        while($m1<=$m2){
+
+             while ( $diai <=  $diaf) {
+                $sql = "INSERT INTO `tb_calendario` (`id_calendario`, `dia`, `mes`, `anio`, `horario`, `id_espacio`) VALUES 
+                (NULL, '".$diai."', '".$m1."', '".$anio."', '".$jornada."', '".$idespacio."');";
+                ////echo $sql."<br>";
+                 try{
+                // Get DB Object
+                $db = new db();
+                // Connect
+                $db = $db->connect();
+                $stmt = $db->prepare($sql);
+                $stmt->execute();
+                //echo '{"notice": {"text": "Customer Added" '.$sql.'}';
+            } catch(PDOException $e){
+                //echo '{"error": {"text": '.$e->getMessage().'}';
+            }
+                $diai = $diai +1;
+             }
+             $diai = 1;
+             $m1 = $m1 +1;
+             $diaf = cuantodistieneelmes($m1);
+             if($m1==$m2 ){
+               $diaf = $d2;
+             }
+        }
+    }elseif($diferenciames!=0 AND $diferenciaanio!=0){
+        $m1 = (int) $mes;$m2 = (int) $mes2;
+        $d1 = (int) $dia;$d2 = (int) $dia2;
+        $a1 = (int) $anio1;$a2 = (int) $anio;
+        $diai = $d1;
+        $diaf = $diasdelmes;
+        $m2f = 12;
+        while ($a1 <= $a2) {
+            while($m1<=$m2f){
+                while ( $diai <=  $diaf) {
+                    $diaa = $diai;
+                    $mess = $m1;
+                    if($diai<10){
+                        $diaa = "0". $diai;
+                    }
+                    if($m1<10){
+                        $mess = "0".$m1;
+                    }
+                    $sql = "INSERT INTO `tb_calendario` (`id_calendario`, `dia`, `mes`, `anio`, `horario`, `id_espacio`) VALUES 
+                    (NULL, '".$diaa."', '".$mess."', '".$a1."', '".$jornada."', '".$idespacio."');";
+                    //echo $sql."<br>";
+                     try{
+                // Get DB Object
+                $db = new db();
+                // Connect
+                $db = $db->connect();
+                $stmt = $db->prepare($sql);
+                $stmt->execute();
+                //echo '{"notice": {"text": "Customer Added" '.$sql.'}';
+                } catch(PDOException $e){
+                    //echo '{"error": {"text": '.$e->getMessage().'}';
+                }
+                        $diai = $diai +1;
+                }
+                     $diai = 1;
+                     //echo $m1."<br>";
+                     $m1 = $m1 +1;
+                     if($m1==13){
+                        $m1 = 1;
+                        $a1 = $a1 +1;
+                    }
+                     $diaf = cuantodistieneelmes($m1);
+                     if($m1==$m2 AND $a1==$a2 ){
+                       $diaf = $d2;
+                       $m2f = $m2;
+                     }
+            }
+            $a1 = $a1 +1;
+        }
+    }
+    
+
+ 
+
+    /*
+    $sql = "SELECT COUNT(*) AS ocupado FROM `tb_temp_usuario` WHERE `id_usuario`='$info'
+           ";
+     try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $result = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+        //echo json_encode($result);
+    } catch(PDOException $e){
+        //echo '{"error": {"text": '.$e->getMessage().'}';
+    }*/
+
+});
+
+   function cuantodistieneelmes($mes)
+    {
+        if($mes == 1 OR $mes == 3 OR $mes == 5 OR $mes == 7 OR $mes == 8 OR $mes == 10 OR $mes == 12){
+        $diasdelmes = 31;
+        }
+        if($mes == 4 OR $mes == 6 OR $mes == 9 OR $mes == 11){
+            $diasdelmes = 30;
+        }
+        if($mes == 2){
+            $diasdelmes = 28;
+        }
+       
+        return $diasdelmes;
+    }
