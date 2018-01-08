7b6b46306fa9f8adaa5990273fc99da35fda6d0f
jarizanativapps <jariza@nativapps.co>
2018-01-08 14:14:24 -0500

    Cortar codifo de otion y headers a index

--- public/index.php
+++ public/index.php
@@ -6,6 +6,16 @@ require '../vendor/autoload.php';
 require '../src/config/db.php';  
 $app = new \Slim\App; 
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
 require '../src/routes/buildings.php'; 
 require '../src/routes/novedades.php'; 
 $app->run();
