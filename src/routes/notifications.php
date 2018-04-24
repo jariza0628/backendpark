5ec8bbbcc3c6134fe6d1234be35ca698bd66a826
jariza0628 <jeffer.junior28@gmail.com>
2018-04-24 09:23:30 -0500

    Endpoin de pruena push header

--- src/routes/notifications.php
+++ src/routes/notifications.php
@@ -1,4 +1,7 @@
 <?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
 function sendMessage($msj){
 		$content = array(
 			"en" => $msj
