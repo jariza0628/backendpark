f2902ac20d152e5252d7f1e24dc8ae55710c1969
Jariza28 <jariz@anativapps.co>
2019-06-18 20:40:37 -0500

    Actualizacion BD

--- public/index.php
+++ public/index.php
@@ -2,8 +2,8 @@
 use \Psr\Http\Message\ServerRequestInterface as Request; 
 use \Psr\Http\Message\ResponseInterface as Response;  
 
-require 'vendor/autoload.php';
-require 'src/config/db.php';  
+require '../vendor/autoload.php';
+require '../src/config/db.php';  
 $app = new \Slim\App; 
 
 $app->options('/{routes:.+}', function ($request, $response, $args) {
