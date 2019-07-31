deed665e6fee841a12e4dd24830eaf70b484e133
Jariza28 <jariz@anativapps.co>
2019-07-31 11:35:09 -0500

    login

--- public/views/login/login.php
+++ public/views/login/login.php
@@ -17,96 +17,191 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     <style type="text/css">
-    	@import "bourbon";
-      body {
-      	background: rgba(146,192,221,1);
-background: -moz-radial-gradient(center, ellipse cover, rgba(146,192,221,1) 0%, rgba(117,174,209,1) 41%, rgba(74,146,191,1) 100%);
-background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%, rgba(146,192,221,1)), color-stop(41%, rgba(117,174,209,1)), color-stop(100%, rgba(74,146,191,1)));
-background: -webkit-radial-gradient(center, ellipse cover, rgba(146,192,221,1) 0%, rgba(117,174,209,1) 41%, rgba(74,146,191,1) 100%);
-background: -o-radial-gradient(center, ellipse cover, rgba(146,192,221,1) 0%, rgba(117,174,209,1) 41%, rgba(74,146,191,1) 100%);
-background: -ms-radial-gradient(center, ellipse cover, rgba(146,192,221,1) 0%, rgba(117,174,209,1) 41%, rgba(74,146,191,1) 100%);
-background: radial-gradient(ellipse at center, rgba(146,192,221,1) 0%, rgba(117,174,209,1) 41%, rgba(74,146,191,1) 100%);
-filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#92c0dd', endColorstr='#4a92bf', GradientType=1 );
+     
+     body {
+    background: #7b8f98;
+    font-family: 'Roboto', sans-serif;
+}
 
-      }
+.login-box {
+    margin-top: 75px;
+    height: auto;
+    background: #1A2226;
+    text-align: center;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+}
 
-      .wrapper {	
-      	margin-top: calc(0px + 13%);
-        margin-bottom: 80px;
-      }
-      @media (max-width: 768px){
-          .wrapper {  
-            margin-top: calc(0px + 40%);
-            margin-bottom: 80px;
-          }
-      }
-      .form-signin {
-        max-width: 380px;
-        padding: 15px 35px 45px;
-        margin: 0 auto;
-        background-color: #fff;
-        border: 1px solid rgba(0,0,0,0.1);  
-        box-shadow: 0px 0px 89px -8px rgba(0,0,0,0.75);
-        .form-signin-heading,
-      	.checkbox {
-      	  margin-bottom: 30px;
-      	}
+.login-key {
+    height: 100px;
+    font-size: 80px;
+    line-height: 100px;
+    background: -webkit-linear-gradient(#27EF9F, #0DB8DE);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
 
-      	.checkbox {
-      	  font-weight: normal;
-      	}
+.login-title {
+    margin-top: 15px;
+    text-align: center;
+    font-size: 30px;
+    letter-spacing: 2px;
+    margin-top: 15px;
+    font-weight: bold;
+    color: #ECF0F5;
+}
 
-      	.form-control {
-      	  position: relative;
-      	  font-size: 16px;
-      	  height: auto;
-      	  padding: 10px;
-      		@include box-sizing(border-box);
+.login-form {
+    margin-top: 25px;
+    text-align: left;
+}
 
-      		&:focus {
-      		  z-index: 2;
-      		}
-      	}
+input[type=text] {
+    background-color: #1A2226;
+    border: none;
+    border-bottom: 2px solid #0DB8DE;
+    border-top: 0px;
+    border-radius: 0px;
+    font-weight: bold;
+    outline: 0;
+    margin-bottom: 20px;
+    padding-left: 0px;
+    color: #ECF0F5;
+}
 
-      	input[type="text"] {
-      	  margin-bottom: -1px;
-      	  border-bottom-left-radius: 0;
-      	  border-bottom-right-radius: 0;
-      	}
+input[type=password] {
+    background-color: #1A2226;
+    border: none;
+    border-bottom: 2px solid #0DB8DE;
+    border-top: 0px;
+    border-radius: 0px;
+    font-weight: bold;
+    outline: 0;
+    padding-left: 0px;
+    margin-bottom: 20px;
+    color: #ECF0F5;
+}
 
-      	input[type="password"] {
-      	  margin-bottom: 20px;
-      	  border-top-left-radius: 0;
-      	  border-top-right-radius: 0;
-      	}
-      }
+.form-group {
+    margin-bottom: 40px;
+    outline: 0px;
+}
 
+.form-control:focus {
+    border-color: inherit;
+    -webkit-box-shadow: none;
+    box-shadow: none;
+    border-bottom: 2px solid #0DB8DE;
+    outline: 0;
+    background-color: #1A2226;
+    color: #ECF0F5;
+}
+
+input:focus {
+    outline: none;
+    box-shadow: 0 0 0;
+}
+
+label {
+    margin-bottom: 0px;
+}
+
+.form-control-label {
+    font-size: 10px;
+    color: #6C6C6C;
+    font-weight: bold;
+    letter-spacing: 1px;
+}
+
+.btn-outline-primary {
+    border-color: #0DB8DE;
+    color: #0DB8DE;
+    border-radius: 0px;
+    font-weight: bold;
+    letter-spacing: 1px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
+
+.btn-outline-primary:hover {
+    background-color: #0DB8DE;
+    right: 0px;
+}
+
+.login-btm {
+    float: left;
+}
+
+.login-button {
+    padding-right: 0px;
+    text-align: right;
+    margin-bottom: 25px;
+}
+
+.login-text {
+    text-align: left;
+    padding-left: 0px;
+    color: #A2A4A4;
+}
+
+.loginbttm {
+    padding: 0px;
+}
     </style>
   </head>
   <body>
-    <div class="wrapper">
-      <form class="form-signin" action="checklogin.php" method="POST">       
-        <h2 class="form-signin-heading">Please login</h2>
-        <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
-        <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
-        
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
-      </form>
-      <div style="max-width: 380px;margin: auto;padding-top: 10px;">
-        <div style="float: left;text-align: center;min-width: 49%;">
-        <a href="">
-            <img src="/views/asset/img/icon-free.png" width="65px" height="60px"><br>
+     
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-2"></div>
+            <div class="col-lg-6 col-md-8 login-box">
+                <div class="col-lg-12 login-key">
+                    <i class="fa fa-key" aria-hidden="true"></i>
+                </div>
+                <div class="col-lg-12 login-title">
+                    Panel Administrativo FreeParking
+                </div>
+
+                <div class="col-lg-12 login-form">
+                    <div class="col-lg-12 login-form">
+                        <form action="checklogin.php" method="POST">
+                            <div class="form-group">
+                                <label class="form-control-label">USUARIO</label>
+                                <input type="text" class="form-control" name="username" required>
+                            </div>
+                            <div class="form-group">
+                                <label class="form-control-label">CONRASEÑA</label>
+                                <input type="password" class="form-control" name="password" required>
+                            </div>
+
+                            <div class="col-lg-12 loginbttm">
+                                <div class="col-lg-6 login-btm login-text">
+                                    <!-- Error Message -->
+                                </div>
+                                <div class="col-lg-6 login-btm login-button">
+                                    <button type="submit" class="btn btn-outline-primary">Entrar</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+ 
+                </div>
+                <div class="col-lg-3 col-md-2"></div>
+            </div>
             
-        </a>
-      </div>
-      <div style="float: right;text-align: center;min-width: 49%;">
-        <a href="">
-            <img src="/views/asset/img/icon-silla.png" width="60px" height="60px"><br>
-            
-        </a>
-      </div>
-      </div>
-    </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-3 col-md-2"></div>
+            <div class="col-lg-6 col-md-8 " style="text-align:center;padding-top:30px">
+                
+
+            <img src="../asset/img/icon-free.png" width="100px" height="90px"><br>
+
+                </div>
+                <div class="col-lg-3 col-md-2"></div>
+            </div>
+
+
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
