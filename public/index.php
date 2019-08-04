21d1200c634e7975027c7a772683426b6dee2bf9
Jariza28 <jariz@anativapps.co>
2019-08-04 17:39:14 -0500

    Rutas dependencias ../

--- public/index.php
+++ public/index.php
@@ -2,8 +2,8 @@
 use \Psr\Http\Message\ServerRequestInterface as Request; 
 use \Psr\Http\Message\ResponseInterface as Response;  
 
-require 'vendor/autoload.php';
-require 'src/config/db.php';  
+require '../vendor/autoload.php';
+require '../src/config/db.php';  
 $app = new \Slim\App; 
 
 $app->options('/{routes:.+}', function ($request, $response, $args) {
@@ -16,10 +16,10 @@ $app->add(function ($req, $res, $next) {
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
-require 'src/routes/notifications.php'; 
-require 'src/routes/buildings.php'; 
-require 'src/routes/reservas.php'; 
-require 'src/routes/novedades.php'; 
-require 'src/routes/millas.php';
+require '../src/routes/notifications.php'; 
+require '../src/routes/buildings.php'; 
+require '../src/routes/reservas.php'; 
+require '../src/routes/novedades.php'; 
+require '../src/routes/millas.php';
 //require '../src/routes/mail.php';  
 $app->run();
